b5fa16607e6679c61fe76a77666425505d7e109e
Maximilian Katzmann <max.katzmann@gmail.com>
2025-02-02 16:19:48 +0100

    Added window indicators to MenuBar

--- Sources/AppBundle/MenuBar.swift
+++ Sources/AppBundle/MenuBar.swift
@@ -3,85 +3,149 @@ import Foundation
 import SwiftUI
 
 public func menuBar(viewModel: TrayMenuModel) -> some Scene {
-    MenuBarExtra {
-        let shortIdentification = "\(aeroSpaceAppName) v\(aeroSpaceAppVersion) \(gitShortHash)"
-        let identification      = "\(aeroSpaceAppName) v\(aeroSpaceAppVersion) \(gitHash)"
-        Text(shortIdentification)
-        Button("Copy to clipboard") { identification.copyToClipboard() }
-            .keyboardShortcut("C", modifiers: .command)
-        Divider()
-        if viewModel.isEnabled {
-            Text("Workspaces:")
-            ForEach(viewModel.workspaces, id: \.name) { workspace in
-                Button {
-                    refreshSession(.menuBarButton, screenIsDefinitelyUnlocked: true) { _ = Workspace.get(byName: workspace.name).focusWorkspace() }
-                } label: {
-                    Toggle(isOn: .constant(workspace.isFocused)) {
-                        Text(workspace.name + workspace.suffix).font(.system(.body, design: .monospaced))
-                    }
-                }
-            }
-            Divider()
+  MenuBarExtra {
+    let shortIdentification = "\(aeroSpaceAppName) v\(aeroSpaceAppVersion) \(gitShortHash)"
+    let identification = "\(aeroSpaceAppName) v\(aeroSpaceAppVersion) \(gitHash)"
+    Text(shortIdentification)
+    Button("Copy to clipboard") { identification.copyToClipboard() }
+      .keyboardShortcut("C", modifiers: .command)
+    Divider()
+    if viewModel.isEnabled {
+      Text("Workspaces:")
+      ForEach(viewModel.workspaces, id: \.name) { workspace in
+        Button {
+          refreshSession(.menuBarButton, screenIsDefinitelyUnlocked: true) {
+            _ = Workspace.get(byName: workspace.name).focusWorkspace()
+          }
+        } label: {
+          Toggle(isOn: .constant(workspace.isFocused)) {
+            Text(workspace.name + workspace.suffix).font(.system(.body, design: .monospaced))
+          }
         }
-        Button(viewModel.isEnabled ? "Disable" : "Enable") {
-            refreshSession(.menuBarButton, screenIsDefinitelyUnlocked: true) {
-                _ = EnableCommand(args: EnableCmdArgs(rawArgs: [], targetState: .toggle)).run(.defaultEnv, .emptyStdin)
-            }
-        }.keyboardShortcut("E", modifiers: .command)
-        let editor = getTextEditorToOpenConfig()
-        Button("Open config in '\(editor.lastPathComponent)'") {
-            let fallbackConfig: URL = FileManager.default.homeDirectoryForCurrentUser.appending(path: configDotfileName)
-            switch findCustomConfigUrl() {
-                case .file(let url):
-                    url.open(with: editor)
-                case .noCustomConfigExists:
-                    _ = try? FileManager.default.copyItem(atPath: defaultConfigUrl.path, toPath: fallbackConfig.path)
-                    fallbackConfig.open(with: editor)
-                case .ambiguousConfigError:
-                    fallbackConfig.open(with: editor)
-            }
-        }.keyboardShortcut("O", modifiers: .command)
-        if viewModel.isEnabled {
-            Button("Reload config") {
-                refreshSession(.menuBarButton, screenIsDefinitelyUnlocked: true) { _ = reloadConfig() }
-            }.keyboardShortcut("R", modifiers: .command)
-        }
-        Button("Quit \(aeroSpaceAppName)") {
-            terminationHandler.beforeTermination()
-            terminateApp()
-        }.keyboardShortcut("Q", modifiers: .command)
-    } label: {
-        if viewModel.isEnabled {
-            MonospacedText(viewModel.trayText)
-        } else {
-            MonospacedText("⏸️")
-        }
+      }
+      Divider()
     }
+    Button(viewModel.isEnabled ? "Disable" : "Enable") {
+      refreshSession(.menuBarButton, screenIsDefinitelyUnlocked: true) {
+        _ = EnableCommand(args: EnableCmdArgs(rawArgs: [], targetState: .toggle)).run(
+          .defaultEnv, .emptyStdin)
+      }
+    }.keyboardShortcut("E", modifiers: .command)
+    let editor = getTextEditorToOpenConfig()
+    Button("Open config in '\(editor.lastPathComponent)'") {
+      let fallbackConfig: URL = FileManager.default.homeDirectoryForCurrentUser.appending(
+        path: configDotfileName)
+      switch findCustomConfigUrl() {
+      case .file(let url):
+        url.open(with: editor)
+      case .noCustomConfigExists:
+        _ = try? FileManager.default.copyItem(
+          atPath: defaultConfigUrl.path, toPath: fallbackConfig.path)
+        fallbackConfig.open(with: editor)
+      case .ambiguousConfigError:
+        fallbackConfig.open(with: editor)
+      }
+    }.keyboardShortcut("O", modifiers: .command)
+    if viewModel.isEnabled {
+      Button("Reload config") {
+        refreshSession(.menuBarButton, screenIsDefinitelyUnlocked: true) { _ = reloadConfig() }
+      }.keyboardShortcut("R", modifiers: .command)
+    }
+    Button("Quit \(aeroSpaceAppName)") {
+      terminationHandler.beforeTermination()
+      terminateApp()
+    }.keyboardShortcut("Q", modifiers: .command)
+  } label: {
+    if viewModel.isEnabled {
+      WindowIconList(workspaceDisplayInfo: Workspace.get(byName: viewModel.trayText).displayInfo)
+    } else {
+      Text("?")
+    }
+  }
 }
 
-struct MonospacedText: View {
-    @Environment(\.colorScheme) var colorScheme: ColorScheme
-    var text: String
-    init(_ text: String) { self.text = text }
+struct SpaceIndicator: View {
+  let name: String
 
-    var body: some View {
-        let renderer = ImageRenderer(
-            content: Text(text)
-                .font(.system(.largeTitle, design: .monospaced))
-                .foregroundStyle(colorScheme == .light ? Color.black : Color.white)
-        )
-        if let cgImage = renderer.cgImage {
-            // Using scale: 1 results in a blurry image for unknown reasons
-            Image(cgImage, scale: 2, label: Text(text))
-        } else {
-            // In case image can't be rendered fallback to plain text
-            Text(text)
-        }
+  var body: some View {
+    switch name {
+    case "1":
+      Image(systemName: "house")
+        .font(.system(.largeTitle))
+    case "2":
+      Image(systemName: "message")
+        .font(.system(.largeTitle))
+    case "3":
+      Image(systemName: "hammer")
+        .font(.system(.largeTitle))
+    case "4":
+      Image(systemName: "hourglass")
+        .font(.system(.largeTitle))
+    case _: Text(name).font(.system(.largeTitle))
     }
+  }
+}
+
+struct WindowIconList: View {
+  @Environment(\.colorScheme) var colorScheme: ColorScheme
+  var workspaceDisplayInfo: WorkspaceDisplayInfo
+
+  var name: String { workspaceDisplayInfo.name }
+  var bundleIdentifiers: [String] { workspaceDisplayInfo.bundleIdentifiers }
+  var focussedIndex: Int? { workspaceDisplayInfo.indexOfFocussed }
+
+  var body: some View {
+    let renderer = ImageRenderer(
+      content:
+        HStack {
+          SpaceIndicator(name: name)
+          Spacer(minLength: 20.0)
+          ForEach(Array(bundleIdentifiers.enumerated()), id: \.offset) { index, bundleIdentifier in
+            WindowIndicator(
+              bundleIdentifier: bundleIdentifier,
+              focussed: index == focussedIndex
+            )
+          }
+        }
+        .foregroundStyle(colorScheme == .light ? Color.black : Color.white)
+    )
+    if let cgImage = renderer.cgImage {
+      // Using scale: 1 results in a blurry image for unknown reasons
+      Image(cgImage, scale: 2, label: Text("?"))
+    } else {
+      // In case image can't be rendered fallback to plain text
+      Text("?")
+    }
+  }
+}
+
+struct WindowIndicator: View {
+  @Environment(\.colorScheme) var colorScheme: ColorScheme
+
+  let bundleIdentifier: String
+  let focussed: Bool
+
+  var body: some View {
+    VStack(spacing: 2.5) {
+      if let app = NSWorkspace.shared.runningApplications.first(where: {
+        $0.bundleIdentifier == bundleIdentifier
+      }),
+        let icon = app.icon
+      {
+        Image(nsImage: icon)
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+      }
+      Rectangle()
+        .fill(focussed ? Color.white : Color.white.opacity(0.25))
+        .frame(height: 5)
+        .cornerRadius(2)
+    }
+  }
 }
 
 func getTextEditorToOpenConfig() -> URL {
-    NSWorkspace.shared.urlForApplication(toOpen: findCustomConfigUrl().urlOrNil ?? defaultConfigUrl)?
-        .takeIf { $0.lastPathComponent != "Xcode.app" } // Blacklist Xcode. It is too heavy to open plain text files
-        ?? URL(filePath: "/System/Applications/TextEdit.app")
+  NSWorkspace.shared.urlForApplication(toOpen: findCustomConfigUrl().urlOrNil ?? defaultConfigUrl)?
+    .takeIf { $0.lastPathComponent != "Xcode.app" }  // Blacklist Xcode. It is too heavy to open plain text files
+    ?? URL(filePath: "/System/Applications/TextEdit.app")
 }
--- Sources/AppBundle/tree/Workspace.swift
+++ Sources/AppBundle/tree/Workspace.swift
@@ -9,184 +9,240 @@ private var visibleWorkspaceToScreenPoint: [Workspace: CGPoint] = [:]
 
 // The returned workspace must be invisible and it must belong to the requested monitor
 func getStubWorkspace(for monitor: Monitor) -> Workspace {
-    getStubWorkspace(forPoint: monitor.rect.topLeftCorner)
+  getStubWorkspace(forPoint: monitor.rect.topLeftCorner)
 }
 
 private func getStubWorkspace(forPoint point: CGPoint) -> Workspace {
-    if let prev = screenPointToPrevVisibleWorkspace[point]?.lets({ Workspace.get(byName: $0) }),
-       !prev.isVisible && prev.workspaceMonitor.rect.topLeftCorner == point && prev.forceAssignedMonitor == nil
-    {
-        return prev
+  if let prev = screenPointToPrevVisibleWorkspace[point]?.lets({ Workspace.get(byName: $0) }),
+    !prev.isVisible && prev.workspaceMonitor.rect.topLeftCorner == point
+      && prev.forceAssignedMonitor == nil
+  {
+    return prev
+  }
+  if let candidate = Workspace.all
+    .first(where: { !$0.isVisible && $0.workspaceMonitor.rect.topLeftCorner == point })
+  {
+    return candidate
+  }
+  let preservedNames = config.preservedWorkspaceNames.toSet()
+  return (1...Int.max).lazy
+    .map { Workspace.get(byName: String($0)) }
+    .first {
+      $0.isEffectivelyEmpty && !$0.isVisible && !preservedNames.contains($0.name)
+        && $0.forceAssignedMonitor == nil
     }
-    if let candidate = Workspace.all
-        .first(where: { !$0.isVisible && $0.workspaceMonitor.rect.topLeftCorner == point })
-    {
-        return candidate
-    }
-    let preservedNames = config.preservedWorkspaceNames.toSet()
-    return (1 ... Int.max).lazy
-        .map { Workspace.get(byName: String($0)) }
-        .first { $0.isEffectivelyEmpty && !$0.isVisible && !preservedNames.contains($0.name) && $0.forceAssignedMonitor == nil }
-        ?? errorT("Can't create empty workspace")
+    ?? errorT("Can't create empty workspace")
+}
+
+struct WorkspaceDisplayInfo {
+  let name: String
+  let bundleIdentifiers: [String]
+  let indexOfFocussed: Int?
 }
 
 class Workspace: TreeNode, NonLeafTreeNodeObject, Hashable, CustomStringConvertible, Comparable {
-    let name: String
-    private let nameLogicalSegments: StringLogicalSegments
-    /// `assignedMonitorPoint` must be interpreted only when the workspace is invisible
-    fileprivate var assignedMonitorPoint: CGPoint? = nil
+  let name: String
+  private let nameLogicalSegments: StringLogicalSegments
+  /// `assignedMonitorPoint` must be interpreted only when the workspace is invisible
+  fileprivate var assignedMonitorPoint: CGPoint? = nil
 
-    private init(_ name: String) {
-        self.name = name
-        self.nameLogicalSegments = name.toLogicalSegments()
-        super.init(parent: NilTreeNode.instance, adaptiveWeight: 0, index: 0)
+  private init(_ name: String) {
+    self.name = name
+    self.nameLogicalSegments = name.toLogicalSegments()
+    super.init(parent: NilTreeNode.instance, adaptiveWeight: 0, index: 0)
+  }
+
+  static var all: [Workspace] {
+    workspaceNameToWorkspace.values.sorted()
+  }
+
+  static func get(byName name: String) -> Workspace {
+    if let existing = workspaceNameToWorkspace[name] {
+      return existing
+    } else {
+      let workspace = Workspace(name)
+      workspaceNameToWorkspace[name] = workspace
+      return workspace
+    }
+  }
+
+  static func < (lhs: Workspace, rhs: Workspace) -> Bool {
+    lhs.nameLogicalSegments < rhs.nameLogicalSegments
+  }
+
+  override func getWeight(_ targetOrientation: Orientation) -> CGFloat {
+    workspaceMonitor.visibleRectPaddedByOuterGaps.getDimension(targetOrientation)
+  }
+
+  override func setWeight(_ targetOrientation: Orientation, _ newValue: CGFloat) {
+    error("It's not possible to change weight of Workspace")
+  }
+
+  var description: String {
+    let preservedNames = config.preservedWorkspaceNames.toSet()
+    let description = [
+      ("name", name),
+      ("isVisible", String(isVisible)),
+      ("isEffectivelyEmpty", String(isEffectivelyEmpty)),
+      ("doKeepAlive", String(preservedNames.contains(name))),
+    ].map { "\($0.0): '\(String(describing: $0.1))'" }.joined(separator: ", ")
+    return "Workspace(\(description))"
+  }
+
+  var displayInfo: WorkspaceDisplayInfo {
+    let windowsWithBundleIdentifiers = self.allLeafWindowsRecursive.filter {
+      $0.asMacWindow().macApp.nsApp.bundleIdentifier != nil
     }
 
-    static var all: [Workspace] {
-        workspaceNameToWorkspace.values.sorted()
+    let bundleIdentifiers = windowsWithBundleIdentifiers.compactMap {
+      $0.asMacWindow().macApp.nsApp.bundleIdentifier
     }
 
-    static func get(byName name: String) -> Workspace {
-        if let existing = workspaceNameToWorkspace[name] {
-            return existing
-        } else {
-            let workspace = Workspace(name)
-            workspaceNameToWorkspace[name] = workspace
-            return workspace
-        }
+    var indexOfFocussed: Int? = nil
+
+    if let focussedWindow = focus.windowOrNil,
+      let index = windowsWithBundleIdentifiers.firstIndex(of: focussedWindow)
+    {
+      indexOfFocussed = index
     }
 
-    static func < (lhs: Workspace, rhs: Workspace) -> Bool { lhs.nameLogicalSegments < rhs.nameLogicalSegments }
+    return WorkspaceDisplayInfo(
+      name: name,
+      bundleIdentifiers: bundleIdentifiers,
+      indexOfFocussed: indexOfFocussed
+    )
+  }
 
-    override func getWeight(_ targetOrientation: Orientation) -> CGFloat {
-        workspaceMonitor.visibleRectPaddedByOuterGaps.getDimension(targetOrientation)
+  var windowsDisplayString: String {
+    let windows = self.allLeafWindowsRecursive
+
+    var windowShorts = windows.map {
+      let name = $0.asMacWindow().macApp.name ?? ""
+      return String(name.replacingOccurrences(of: "\u{200E}", with: "").prefix(2))
     }
 
-    override func setWeight(_ targetOrientation: Orientation, _ newValue: CGFloat) {
-        error("It's not possible to change weight of Workspace")
+    if let focussedWindow = focus.windowOrNil,
+      let indexOfFocussed = windows.firstIndex(of: focussedWindow)
+    {
+      windowShorts[indexOfFocussed] = "(" + windowShorts[indexOfFocussed] + ")"
     }
 
-    var description: String {
-        let preservedNames = config.preservedWorkspaceNames.toSet()
-        let description = [
-            ("name", name),
-            ("isVisible", String(isVisible)),
-            ("isEffectivelyEmpty", String(isEffectivelyEmpty)),
-            ("doKeepAlive", String(preservedNames.contains(name))),
-        ].map { "\($0.0): '\(String(describing: $0.1))'" }.joined(separator: ", ")
-        return "Workspace(\(description))"
-    }
+    return windowShorts.joined(separator: " ")
+  }
 
-    static func garbageCollectUnusedWorkspaces() {
-        let preservedNames = config.preservedWorkspaceNames.toSet()
-        for name in preservedNames {
-            _ = get(byName: name) // Make sure that all preserved workspaces are "cached"
-        }
-        workspaceNameToWorkspace = workspaceNameToWorkspace.filter { (_, workspace: Workspace) in
-            preservedNames.contains(workspace.name) ||
-                !workspace.isEffectivelyEmpty ||
-                workspace.isVisible ||
-                workspace.name == focus.workspace.name
-        }
+  static func garbageCollectUnusedWorkspaces() {
+    let preservedNames = config.preservedWorkspaceNames.toSet()
+    for name in preservedNames {
+      _ = get(byName: name)  // Make sure that all preserved workspaces are "cached"
     }
+    workspaceNameToWorkspace = workspaceNameToWorkspace.filter { (_, workspace: Workspace) in
+      preservedNames.contains(workspace.name) || !workspace.isEffectivelyEmpty
+        || workspace.isVisible || workspace.name == focus.workspace.name
+    }
+  }
 
-    static func == (lhs: Workspace, rhs: Workspace) -> Bool {
-        check((lhs === rhs) == (lhs.name == rhs.name), "lhs: \(lhs) rhs: \(rhs)")
-        return lhs === rhs
-    }
+  static func == (lhs: Workspace, rhs: Workspace) -> Bool {
+    check((lhs === rhs) == (lhs.name == rhs.name), "lhs: \(lhs) rhs: \(rhs)")
+    return lhs === rhs
+  }
 
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(name)
-    }
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(name)
+  }
 }
 
 extension Workspace {
-    var isVisible: Bool { visibleWorkspaceToScreenPoint.keys.contains(self) }
-    var workspaceMonitor: Monitor {
-        forceAssignedMonitor
-            ?? visibleWorkspaceToScreenPoint[self]?.monitorApproximation
-            ?? assignedMonitorPoint?.monitorApproximation
-            ?? mainMonitor
-    }
+  var isVisible: Bool { visibleWorkspaceToScreenPoint.keys.contains(self) }
+  var workspaceMonitor: Monitor {
+    forceAssignedMonitor
+      ?? visibleWorkspaceToScreenPoint[self]?.monitorApproximation
+      ?? assignedMonitorPoint?.monitorApproximation
+      ?? mainMonitor
+  }
 }
 
 extension Monitor {
-    var activeWorkspace: Workspace {
-        if let existing = screenPointToVisibleWorkspace[rect.topLeftCorner] {
-            return existing
-        }
-        // What if monitor configuration changed? (frame.origin is changed)
-        rearrangeWorkspacesOnMonitors()
-        // Normally, recursion should happen only once more because we must take the value from the cache
-        // (Unless, monitor configuration data race happens)
-        return self.activeWorkspace
+  var activeWorkspace: Workspace {
+    if let existing = screenPointToVisibleWorkspace[rect.topLeftCorner] {
+      return existing
     }
+    // What if monitor configuration changed? (frame.origin is changed)
+    rearrangeWorkspacesOnMonitors()
+    // Normally, recursion should happen only once more because we must take the value from the cache
+    // (Unless, monitor configuration data race happens)
+    return self.activeWorkspace
+  }
 
-    func setActiveWorkspace(_ workspace: Workspace) -> Bool {
-        rect.topLeftCorner.setActiveWorkspace(workspace)
-    }
+  func setActiveWorkspace(_ workspace: Workspace) -> Bool {
+    rect.topLeftCorner.setActiveWorkspace(workspace)
+  }
 }
 
 func gcMonitors() {
-    if screenPointToVisibleWorkspace.count != monitors.count {
-        rearrangeWorkspacesOnMonitors()
-    }
+  if screenPointToVisibleWorkspace.count != monitors.count {
+    rearrangeWorkspacesOnMonitors()
+  }
 }
 
-private extension CGPoint {
-    func setActiveWorkspace(_ workspace: Workspace) -> Bool {
-        if !isValidAssignment(workspace: workspace, screen: self) {
-            return false
-        }
-        if let prevMonitorPoint = visibleWorkspaceToScreenPoint[workspace] {
-            visibleWorkspaceToScreenPoint.removeValue(forKey: workspace)
-            screenPointToPrevVisibleWorkspace[prevMonitorPoint] =
-                screenPointToVisibleWorkspace.removeValue(forKey: prevMonitorPoint)?.name
-        }
-        if let prevWorkspace = screenPointToVisibleWorkspace[self] {
-            screenPointToPrevVisibleWorkspace[self] =
-                screenPointToVisibleWorkspace.removeValue(forKey: self)?.name
-            visibleWorkspaceToScreenPoint.removeValue(forKey: prevWorkspace)
-        }
-        visibleWorkspaceToScreenPoint[workspace] = self
-        screenPointToVisibleWorkspace[self] = workspace
-        workspace.assignedMonitorPoint = self
-        return true
+extension CGPoint {
+  fileprivate func setActiveWorkspace(_ workspace: Workspace) -> Bool {
+    if !isValidAssignment(workspace: workspace, screen: self) {
+      return false
     }
+    if let prevMonitorPoint = visibleWorkspaceToScreenPoint[workspace] {
+      visibleWorkspaceToScreenPoint.removeValue(forKey: workspace)
+      screenPointToPrevVisibleWorkspace[prevMonitorPoint] =
+        screenPointToVisibleWorkspace.removeValue(forKey: prevMonitorPoint)?.name
+    }
+    if let prevWorkspace = screenPointToVisibleWorkspace[self] {
+      screenPointToPrevVisibleWorkspace[self] =
+        screenPointToVisibleWorkspace.removeValue(forKey: self)?.name
+      visibleWorkspaceToScreenPoint.removeValue(forKey: prevWorkspace)
+    }
+    visibleWorkspaceToScreenPoint[workspace] = self
+    screenPointToVisibleWorkspace[self] = workspace
+    workspace.assignedMonitorPoint = self
+    return true
+  }
 }
 
 private func rearrangeWorkspacesOnMonitors() {
-    var oldVisibleScreens: Set<CGPoint> = screenPointToVisibleWorkspace.keys.toSet()
+  var oldVisibleScreens: Set<CGPoint> = screenPointToVisibleWorkspace.keys.toSet()
 
-    let newScreens = monitors.map(\.rect.topLeftCorner)
-    var newScreenToOldScreenMapping: [CGPoint: CGPoint] = [:]
-    for newScreen in newScreens {
-        if let oldScreen = oldVisibleScreens.minBy({ ($0 - newScreen).vectorLength }) {
-            check(oldVisibleScreens.remove(oldScreen) != nil)
-            newScreenToOldScreenMapping[newScreen] = oldScreen
-        }
+  let newScreens = monitors.map(\.rect.topLeftCorner)
+  var newScreenToOldScreenMapping: [CGPoint: CGPoint] = [:]
+  for newScreen in newScreens {
+    if let oldScreen = oldVisibleScreens.minBy({ ($0 - newScreen).vectorLength }) {
+      check(oldVisibleScreens.remove(oldScreen) != nil)
+      newScreenToOldScreenMapping[newScreen] = oldScreen
     }
+  }
 
-    let oldScreenPointToVisibleWorkspace = screenPointToVisibleWorkspace
-    screenPointToVisibleWorkspace = [:]
-    visibleWorkspaceToScreenPoint = [:]
+  let oldScreenPointToVisibleWorkspace = screenPointToVisibleWorkspace
+  screenPointToVisibleWorkspace = [:]
+  visibleWorkspaceToScreenPoint = [:]
 
-    for newScreen in newScreens {
-        if let existingVisibleWorkspace = newScreenToOldScreenMapping[newScreen]?.lets({ oldScreenPointToVisibleWorkspace[$0] }),
-           newScreen.setActiveWorkspace(existingVisibleWorkspace)
-        {
-            continue
-        }
-        let stubWorkspace = getStubWorkspace(forPoint: newScreen)
-        check(newScreen.setActiveWorkspace(stubWorkspace),
-              "getStubWorkspace generated incompatible stub workspace (\(stubWorkspace)) for the monitor (\(newScreen)")
+  for newScreen in newScreens {
+    if let existingVisibleWorkspace = newScreenToOldScreenMapping[newScreen]?.lets({
+      oldScreenPointToVisibleWorkspace[$0]
+    }),
+      newScreen.setActiveWorkspace(existingVisibleWorkspace)
+    {
+      continue
     }
+    let stubWorkspace = getStubWorkspace(forPoint: newScreen)
+    check(
+      newScreen.setActiveWorkspace(stubWorkspace),
+      "getStubWorkspace generated incompatible stub workspace (\(stubWorkspace)) for the monitor (\(newScreen)"
+    )
+  }
 }
 
 private func isValidAssignment(workspace: Workspace, screen: CGPoint) -> Bool {
-    if let forceAssigned = workspace.forceAssignedMonitor, forceAssigned.rect.topLeftCorner != screen {
-        return false
-    } else {
-        return true
-    }
+  if let forceAssigned = workspace.forceAssignedMonitor, forceAssigned.rect.topLeftCorner != screen
+  {
+    return false
+  } else {
+    return true
+  }
 }
