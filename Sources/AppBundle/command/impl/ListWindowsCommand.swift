884466a6f8da5c3301aa564398682184b8164d68
Maximilian Katzmann <max.katzmann@gmail.com>
2025-04-28 20:11:57 +0200

    Disable sorting in ListWindowsCommand

--- Sources/AppBundle/command/impl/ListWindowsCommand.swift
+++ Sources/AppBundle/command/impl/ListWindowsCommand.swift
@@ -49,7 +49,6 @@ struct ListWindowsCommand: Command {
                 _list.append((window, try await window.title))
             }
             _list = _list.filter { $0.window.isBound }
-            _list = _list.sortedBy([{ $0.window.app.name ?? "" }, \.title])
 
             let list = _list.map { AeroObj.window(window: $0.window, title: $0.title) }
             if args.json {
